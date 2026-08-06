fea97f3417a3a5907d2133bdc5663dbeade2e61a
ankitashankar <ankitashankar678@gmail.com>
2026-08-06 22:40:58 +0530

    Delete institutions.js

--- institutions.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { getInstitutions, addInstitution } = require('../controllers/institutionController');
-
-router.get('/', getInstitutions);
-router.post('/', addInstitution);
-
-module.exports = router;
